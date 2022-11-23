4b0842c01151f6eefa6b40992bbeb4832f7cbd5c
Dion Dresschers <dion@dresschers.net>
2022-11-23 18:07:10 +0100

    Added Page Model for database

--- OpenKarmaCMS/app/Models/User.php
+++ OpenKarmaCMS/app/Models/User.php
@@ -41,4 +41,10 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    # DD: This has a User to Pages relation:
+    public function pages() {
+        return $this->hadMany('App\Page');
+    }
+
 }
